f97a05b95d523cc796331f6ae2cf0399e6b33468
akshit <akshit.singh20@gmail.com>
2021-01-07 08:09:57 +0530

    final_version

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <cmath>
 #include <stdlib.h>
 #include <string>
-#include"point.hpp"
-#include"point_print.hpp"
+#include "point.hpp"
+#include "point_print.hpp"
+#include "tsp_solver.hpp"
 using namespace std;
 
 int main()
 {
-    
+
     vector<point> array;
     cout << "enter the number of points " << endl;
     int number;
@@ -17,7 +18,7 @@ int main()
     cout << "enter points in the format => point_name x y " << endl;
 
     for (int i = 0; i < number; i++)
-    { 
+    {
 
         int a, b;
         string s;
@@ -25,28 +26,21 @@ int main()
         cin >> a;
         cin >> b;
 
-
-        point defau(a,b,s);
+        point defau(a, b, s);
 
         array.push_back(defau);
-
-
     }
-
-
-
-
 
     system("CLS");
 
-
-    for(int i =0;i<number;i++){
+    for (int i = 0; i < number; i++)
+    {
         array[i].showPoint();
-            }
-
+    }
 
     ListOfPoints(array);
-    
+
+    shortest_path(array);
 
     //example
     return 0;
